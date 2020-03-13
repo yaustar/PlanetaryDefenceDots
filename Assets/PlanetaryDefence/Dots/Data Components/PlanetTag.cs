247fa350c15c08976bb85e3588b867224acab63b
Steven Yau <social@stevenyau.co.uk>
2020-03-13 14:26:05 +0000

    Red enemies now are destroyed when entering the planet trigger area

--- Assets/PlanetaryDefence/Dots/Data Components/PlanetTag.cs
+++ Assets/PlanetaryDefence/Dots/Data Components/PlanetTag.cs
@@ -1,9 +1,4 @@
 ﻿using Unity.Entities;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-
 [GenerateAuthoringComponent]
 public struct PlanetTag : IComponentData {
 
---
+++ Assets/PlanetaryDefence/Dots/Systems/HandlePlanetCollisionsSystem.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Physics;
+using Unity.Physics.Systems;
+using UnityEngine;
+
+[UpdateAfter(typeof(EndFramePhysicsSystem))]
+public class HandlePlanetCollisionsSystem : JobComponentSystem {
+    
+    private struct TriggerJob : ITriggerEventsJob {
+
+        public EntityCommandBuffer commandBuffer;
+
+        public ComponentDataFromEntity<DamagableData> damagableEntities;
+        [ReadOnly] public ComponentDataFromEntity<PlanetTag> planetEntities;  
+        
+        
+        public void Execute(TriggerEvent triggerEvent) {
+            Entity entityA = triggerEvent.Entities.EntityA;
+            Entity entityB = triggerEvent.Entities.EntityB;
+
+            bool planetIsEntityA = planetEntities.Exists(entityA);
+            bool planetIsEntityB = planetEntities.Exists(entityB);
+
+            bool damagableIsEntityA = damagableEntities.Exists(entityA);
+            bool damagebleIsEntityB = damagableEntities.Exists(entityB);
+
+            if (planetIsEntityA && damagebleIsEntityB) {
+                commandBuffer.DestroyEntity(entityB);
+
+            } else if (planetIsEntityB && damagableIsEntityA) {
+                commandBuffer.DestroyEntity(entityA);
+            }
+        }
+    }
+
+
+    private BuildPhysicsWorld _buildPhysicsWorld;
+    private StepPhysicsWorld _stepPhysicsWorld;
+
+    private EndSimulationEntityCommandBufferSystem _ecb;
+
+    
+    protected override void OnCreate() {
+        _buildPhysicsWorld = World.GetOrCreateSystem<BuildPhysicsWorld>();
+        _stepPhysicsWorld = World.GetOrCreateSystem<StepPhysicsWorld>();
+        
+        _ecb = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+    }
+
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps) {
+        
+        TriggerJob triggerJob = new TriggerJob {
+            commandBuffer = _ecb.CreateCommandBuffer(),
+            damagableEntities = GetComponentDataFromEntity<DamagableData>(),
+            planetEntities = GetComponentDataFromEntity<PlanetTag>()
+        };
+        
+        
+        JobHandle triggerJobHandle = triggerJob.Schedule(_stepPhysicsWorld.Simulation, ref _buildPhysicsWorld.PhysicsWorld, inputDeps);
+        _ecb.AddJobHandleForProducer(triggerJobHandle);
+
+        return triggerJobHandle;
+    }
+}
